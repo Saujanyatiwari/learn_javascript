45f4219a72be02c9b13e43f87517ca003795ab3b
Saujanyatiwari <136081916+Saujanyatiwari@users.noreply.github.com>
2024-04-11 18:44:30 +0000

    summary of datatypes

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+//PRIMIIVE
+
+// 7 types : String , Number , Boolean , Null , Undefined , Symbol , BigInt
+
+//NON - PRIMITIVE 
+
+// Arrays , Objects , Functions
+
+let name = "abc"
+
+/* Javascript is DYNAMICALLY TYPED LANGUAGE as we dont assign datatypes and at 
+ runtime the compiler detectes itself*/
+
+ const id = Symbol('123')
+ const nameId = Symbol('123')
+
+ console.log(id == nameId)
+
+ // false will be printed even though the number given is same but the output from the symbol is different
+
+ const heros = ["ironman" , "scarlet_witch" ,"hawkeye"];
+
+ let myobj = {
+    name : "abc",
+    age : 18,
+ }
+
+const myfunctin = function(){
+    console.log("hello world")
+}
+
+
+//corresponding data values from typeof 
+
+//undefined => undefined
+//null => object
+//boolean => boolean
+//string => string
