07efe25e387106a4384e99d1474d989ea958832e
Saujanyatiwari <136081916+Saujanyatiwari@users.noreply.github.com>
2024-04-11 18:01:30 +0000

    operation in js

---
+++ basics/operations.js
@@ -0,0 +1,26 @@
+let value = 3;
+let negvalue = -value;
+
+/*console.log(2+2)
+console.log(4-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)*/
+
+/*console.log("1" + 2)
+console.log(1 +"2")
+console.log("1" + 2 + 2) // first string everything treated as string
+console.log(1 + 2 + "2")// last string then first operation will be performed
+
+console.log( (3+4)* 5 % 3)*/
+
+let n = "hello"
+let s = " world"
+
+let p = n+s
+//console.log(p)
+
+//console.log(+true)
+// prints the boolean value of true when plus sign used
+
