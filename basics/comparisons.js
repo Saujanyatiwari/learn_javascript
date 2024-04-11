505ebd933d58978c583807ed211e4eb73d52800d
Saujanyatiwari <136081916+Saujanyatiwari@users.noreply.github.com>
2024-04-11 18:22:40 +0000

    comapring numbers and data types

---
+++ basics/comparisons.js
@@ -0,0 +1,26 @@
+/* console.log(2>1)
+console.log(2<1)
+console.log(2>=1)
+console.log(2>=1)
+console.log(2==1)
+console.log(2!=1) */
+
+//console.log("2" > 1)
+//concole.log("02" > 1)
+
+// always remember to compare two similar data types
+
+/* console.log(null>0)
+console.log(null==0)
+console.log(null>=0) */
+
+// equality [==] and comaprison[>,<,<=,>=] signs work differenlty
+// comparisons convert null to number so its 0 thus null>0 is false and null>=0 is true
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+//=== this strictly compares the data types even 
+
+console.log("02" === 1)
