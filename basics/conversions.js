a4f31dbf4be6545793720ba4fb840495c1064274
Saujanyatiwari <136081916+Saujanyatiwari@users.noreply.github.com>
2024-04-11 08:14:34 +0000

    conversion of data types

---
+++ basics/conversions.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+console.log(typeof score);
+
+let scores = Number(score);
+console.log(typeof scores);
+console.log(scores);
+
+//output given when converted to number
+//"33" => 33
+// "abc" => NaN
+//null => 0
+//undefined => NaN
+//true/false => 1/0
+
+let loggedIn = 1
+let stringname = 33
+
+let booleanLog = Boolean(loggedIn)
+let names = String(stringname)
+
+console.log(typeof booleanLog);
+console.log(typeof names)
