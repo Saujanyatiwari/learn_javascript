8866f04e4244301bb96ba9b206a4b9934f4601a7
Saujanyatiwari <136081916+Saujanyatiwari@users.noreply.github.com>
2024-04-11 07:58:08 +0000

    variables and data types introduced

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // used not browser not on node.js
+
+let name ="saujanya"
+let age = 18
+let female = true
+
+//datatypes
+//number => 2 to the power 53
+//string => "saujanya"
+//bigint => for extremely large values
+//boolean => true/false
+// null
+//undefined
+
+//object
+
+let state = null // type of null is OBJECT
+let toy;
+
+console.log(typeof female)
+console.log(typeof age)
+console.log(typeof state)
+console.log(typeof toy)
---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let emailId = "st@gmail.com"
+var numberId = "4353436"
+let accountstate
+// const keyword never changes where as let and var can be changed
+//var is not used now a days due to issue in block scope and fucntional scope
+// when nothing assigned then undefined is the output 
+console.table([accountId,emailId,numberId,accountstate])
