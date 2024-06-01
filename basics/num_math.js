3690fdc3f7a3bdaa811981ce1860180c4b2c2a7c
Saujanya Tiwari <136081916+Saujanyatiwari@users.noreply.github.com>
2024-06-01 14:46:14 +0000

    nums and maths file created

---
+++ basics/num_math.js
@@ -0,0 +1,42 @@
+const num = 2000
+// console.log(num)
+
+const nums = new Number(400000)
+// console.log(nums)
+
+
+// console.log(nums.toString().length)
+// console.log(nums.toLocaleString('en-IN'))
+
+const  n = 456.278
+
+// console.log(n.toFixed(1))
+
+// console.log(n.toPrecision(4))
+// console.log(n.toPrecision(2))
+
+//use prescision carefully because it takes the the number passed and exponents the lefts one 
+
+
+//++++++++++++++++++++++++++++++
+
+
+
+/* console.log(Math.abs(-4))
+
+console.log (Math.max(3 , 9 , 1 ,5 ,6))
+console.log (Math.min(3 , 9 , 1 ,5 ,6))
+
+console.log (Math.round(3.5))
+console.log (Math.ceil(3.5))
+console.log (Math.floor(3.5)) */
+
+
+
+console.log(Math.random())
+console.log(Math.floor((Math.random()*10) +1))
+
+min = 10
+max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
