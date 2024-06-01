712c66baef975a694be38814c4349637357b1224
Saujanya Tiwari <136081916+Saujanyatiwari@users.noreply.github.com>
2024-06-01 11:34:55 +0000

    updation

--- basics/stack-heap.js
+++ basics/stack-heap.js
@@ -2,9 +2,32 @@
 
 // heap => non-primitive (arrays , maps) [passes the refernce where the value stored]
 
-let x = apple
-let y = x
-y = mango
+
+// primitive data type
+
+let x = "apple";
+let y = x;
+y = "mango";
+
 
 console.log(x);
 console.log(y);
+
+/*The value for x and y will be different because a copy is tsored in y mt the original value */
+
+// Non-primitive data types
+
+let userone = {
+    email :"abc@google.com",
+    age :19 
+           }
+ 
+let usertwo = userone;
+
+usertwo.email = "xyz@google.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
+
+/* in the above the output for both will be same as it gets a refernve in heap thus when once a value changed
+, it changes for all the objects being referred there */
---
+++ basics/strings.js
@@ -0,0 +1,29 @@
+let name = "saujanya"
+let age = 18
+
+// console.log(name + age + "is age")
+// now the above method not used because of its less readibility
+
+//string interpolation used 
+
+// console.log(`hello my name is ${name} and my age is ${age}`)
+
+//we use this interpolation way because string manipulation becomes easier
+
+const gamename = new String("Saujanya")
+
+// console.log(gamename)
+// console.log(typeof gamename)
+
+// string acts as an object here
+
+// various manipulations can be performend on string usng various methods
+
+// console.log(gamename.length)
+// console.log(gamename.toLowerCase())
+// console.log(gamename.toUpperCase())
+// console.log(gamename.charAt(6))
+// console.log(gamename.indexOf('a'))
+
+/* INDEXOF will return the first position of the element where ever  
+it comes even if it occurs more than one time */
