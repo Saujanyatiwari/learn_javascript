40d1e95348109941f3ac0c1497a946b32b999aaa
Saujanya Tiwari <136081916+Saujanyatiwari@users.noreply.github.com>
2024-06-03 12:23:34 +0000

    object file updation

---
+++ basics_02/objects.js
@@ -0,0 +1,38 @@
+// SINGLETON : ALLWAYS INITIASED WHEN CONSTRUCTOR DECLARED
+// object.create => way for defining a singleton object
+
+
+// object literals
+
+const sym = Symbol("abcd")
+
+
+let jsuser = {
+    name : "Saujanya",
+    "sur name": "Tiwari",
+    [sym] : 9981,
+    age : 18,
+    email: "saujanya@google.com",
+    loggedIn : false,
+    lastLogIn : ["Monday" , "Tuesday"]
+}
+
+/* console.log(jsuser.name)
+console.log(jsuser["name"])    second way
+console.log(jsuser["sur name"])
+console.log(jsuser[sym]) */
+
+// when key is given in string forms then second method is used always
+
+jsuser.loggedIn = true
+// Object.freeze(jsuser)
+
+// freeze commands freezes the whole object hence no changes can be made 
+
+// console.log(jsuser)
+
+jsuser.greetings = function(){
+    console.log("hello user")
+}
+
+console.log(jsuser.greetings)
