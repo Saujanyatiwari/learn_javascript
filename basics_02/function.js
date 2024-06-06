e52d00a417af4c5a2b814c36fe66186040d5588a
Saujanya Tiwari <136081916+Saujanyatiwari@users.noreply.github.com>
2024-06-06 14:36:06 +0000

    updation

--- basics_02/function.js
+++ basics_02/function.js
@@ -10,12 +10,25 @@ function myName(){
 // myName()
 
 function add(n1 , n2)  {
-    console.log(n1+n2)
-    return(n1+n2)
+    console.log(n1+n2) // anything written above return statement shall execute
+    return n1+n2 // it only prints bot returns back return is another concept so return is used 
+    // console.log("saujanya") anything written after return is not considered 
 }
 // add(2,6)
 
-const result = add(2,6)
+// const result = add(2,6)
+// console.log("RESULT : "+result)
 
-console.log("RESULT : "+result)
+function userlogIn(name = "sun") {
+    // if(name === undefined){
+    //     console.log("Please ,enter a user name")
+    //     return
+    // }
+    if(!name){
+        console.log("Please , enter a name")
+        return
+    }
+    return `${name} just logged in`;
+}
 
+console.log(userlogIn("Saujanya"))
