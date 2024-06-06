c9b05c862f3edd1088c1b738382ee87037805c7a
Saujanya Tiwari <136081916+Saujanyatiwari@users.noreply.github.com>
2024-06-06 04:38:10 +0000

    function file created

---
+++ basics_02/function.js
@@ -0,0 +1,21 @@
+
+
+function myName(){
+    console.log("A");
+    console.log("P");
+    console.log("P");
+    console.log("L");
+    console.log("E");
+}
+// myName()
+
+function add(n1 , n2)  {
+    console.log(n1+n2)
+    return(n1+n2)
+}
+// add(2,6)
+
+const result = add(2,6)
+
+console.log("RESULT : "+result)
+
